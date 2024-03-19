b7de3a4db1e92a8cf7e22e5e0c2d46c73b35cfbb
Tiltet <timofeysavinich@mail.ru>
2024-03-20 00:04:52 +0300

    feat: add interface

---
+++ interface/interface.h
@@ -0,0 +1,10 @@
+//
+// Created by Тимофей Савинич on 19.03.24.
+//
+
+#ifndef COURSE_WORK_INTERFACE_H
+#define COURSE_WORK_INTERFACE_H
+
+void interface();
+
+#endif //COURSE_WORK_INTERFACE_H
--- main.cpp
+++ main.cpp
@@ -1,35 +1,13 @@
 #include <iostream>
 #include <getopt.h>
-#include <SFML/Graphics.hpp>
 
 #include "server/server.h"
 #include "client/client.h"
+#include "interface/interface.h"
 
 int main(int argc, char *argv[])
 {
-    // SFML test code
-    sf::RenderWindow window(sf::VideoMode(640, 480), "SFML Application");
-    sf::CircleShape shape;
-    shape.setRadius(40.f);
-    shape.setPosition(100.f, 100.f);
-    shape.setFillColor(sf::Color::Cyan);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-    // SFML test code
-
+    interface(); // начальная функция отрисовки интерфейса
 
     int option;
     int s_option = 0;
