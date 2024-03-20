b5d200968de1dbcb662e9ce6cc5e2f9b6f0fd835
Tiltet <timofeysavinich@mail.ru>
2024-03-20 16:37:56 +0300

    feat: SORRY NIKITA

--- interface/interface.cpp
+++ interface/interface.cpp
@@ -7,41 +7,5 @@
 
 void interface()
 {
-    // SFML test code
-    sf::RenderWindow window(sf::VideoMode(1240, 720), "Sea Battle");
 
-    // Создание шрифта для заголовка
-    sf::Font font;
-    if (!font.loadFromFile("../interface/fonts/Arial Unicode.ttf"))
-    {
-        return;
-    }
-
-    // Создание текста заголовка
-    sf::Text titleText("Sea Battle", font, 24);
-    titleText.setFillColor(sf::Color::White);
-
-    // Получение размеров текста
-    sf::FloatRect textBounds = titleText.getGlobalBounds();
-
-    // Центрирование текста
-    float x = (window.getSize().x - textBounds.width) / 2.0f;
-    float y = (window.getSize().y - textBounds.height) / 2.0f;
-    titleText.setPosition(x,y);
-
-    // ОСНОВНОЙ ЦИКЛ ПРИЛОЖЕНИЯ
-    while (window.isOpen())
-    {
-        sf::Event event;
-
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear(sf::Color::Black);
-        window.draw(titleText);
-        window.display();
-    }
 }
