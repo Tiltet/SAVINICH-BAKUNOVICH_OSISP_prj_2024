47cf1e6ba0218960500e70864ac0dbcbf2482853
Tiltet <timofeysavinich@mail.ru>
2024-03-22 01:05:49 +0300

    feat: add map + add event + click on Map + add CMakeLists.txt for Linux

--- interface/interface.h
+++ interface/interface.h
@@ -5,6 +5,39 @@
 #ifndef COURSE_WORK_INTERFACE_H
 #define COURSE_WORK_INTERFACE_H
 
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <optional>
+
+// Класс с координатми выстрела
+// опциональный так как User может не попасть по полю
+// если пользователь не попал - придут std::nullopt в полях
+struct ShootCoordinates
+{
+    std::optional<int> x;
+    std::optional<int> y;
+};
+
+// #TODO
+// Класс информации о поле
+enum class CellState
+{
+    Empty,
+    Ship,
+    Hit,
+    Miss
+};
+
+
+// Класс поля
+struct Cell
+{
+    CellState state;
+    sf::RectangleShape shape;
+};
+
 void interface();
+ShootCoordinates shoot(sf::RenderWindow& window, std::vector<std::vector<Cell>>& map);
+void drawMap(sf::RenderWindow& window, const std::vector<std::vector<Cell>>& map);
 
 #endif //COURSE_WORK_INTERFACE_H
